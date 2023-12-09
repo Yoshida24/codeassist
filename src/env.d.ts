f833afd028396af0bc9576a62451abfb5540a812
Yoshida24 <trihead1.dev@gmail.com>
2023-12-09 23:14:27 +0900

    feat: OpenAI API Key をブラウザ内でのみ保存

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,7 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_MICROCMS_SERVICE: string;
   readonly VITE_MICROCMS_KEY: string;
-  readonly VITE_OPENAI_KEY: string;
+  // readonly VITE_OPENAI_KEY: string;
   readonly VITE_OPENAI_DEFAULT_MODEL: string;
 }
 
